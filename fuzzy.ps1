9cc646aba6954af99b83ce36f0da40bb003d2d1a
shinyanger <shinyanger@outlook.com>
2023-02-14 11:04:18 +0800

    add back field index restriction for searching

--- fuzzy.ps1
+++ fuzzy.ps1
@@ -87,6 +87,7 @@ function ListDirectory {
     )
     if ($s_settings.showDetails) {
         $fzfParams.Add("--header-lines=2")
+        $fzfParams.Add("--nth=5..")
     }
     if ($s_settings.preview) {
         if ($s_settings.showDetails) {
